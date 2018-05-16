099d363bdf7368723d575cfc52ca9f62cf52e782
Aaron Pankratz <pankratz.aaron@gmail.com>
2018-05-16 11:18:40 -0500

    updated request exports to use browser api which is supported in react native

--- src/request/request.js
+++ src/request/request.js
@@ -1,6 +1,1 @@
-var isBrowser = typeof window !== 'undefined';
-
-// FIXME: how to do conditional loading this with ES6 modules?
-module.exports = isBrowser ?
-    require('./request.browser') :
-    require('./request.node');
+module.exports = require('./request.browser');
